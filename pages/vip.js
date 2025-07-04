6ffb45c4e807d1d2558333fd283fc193f423c9f3
NEXTGEN <157381692+Nextgen112@users.noreply.github.com>
2025-07-04 22:43:27 +0300

    Create vip.js

---
+++ pages/vip.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function VIP() {
+  return (
+    <>
+      <Head>
+        <script src="/VIP.js" defer></script>
+      </Head>
+      <div>VIP Panel loading...</div>
+    </>
+  );
+}
